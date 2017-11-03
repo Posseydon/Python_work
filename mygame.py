9f1bed444fa978ff7f0dd1fb596f7cb810a742e0
Posseydon <posseydon87@gmail.com>
2017-11-03 12:35:47 +0200

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -1,12 +1,15 @@
 from random import randint
 print ("Hello, this is my first game")
 
-player = str(raw_input ('your name: '))
+player = input ('your name: ')
+print("Hello" + " " + player)
+print("Let's try will play")
 
-print("Hello {}").format(player)
-first_step = str(raw_input("Do yo want to start game? y/n:"))
+first_step = str(input("Do yo want to start game? y/n:"))
 
 if first_step == "y":
 	print("You go to old forest, and you will have two ways: ")
 else:
 	print("You do not continue the game, ok see you later")
+
+
