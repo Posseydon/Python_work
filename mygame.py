e51a03fa0e4be733b2ffdc6c1165250f6d0bd853
Posseydon <posseydon87@gmail.com>
2017-11-12 19:53:35 +0200

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -1,91 +1,75 @@
-from random import randint
-print ("Hello, this is my first game")
+import random
 
+print ("Hello, this is my first game")
+#What is your name?
 player = input ('your name: ')
+#we glade to see you
 print("Hello" + " " + player)
 print("Let's try will play")
+#Our game is strting
+def start(first_step):
+	if first_step == "y":
+		return("You go to old forest, and you will have two ways: ")
+	quit()
+start(str(input("Do yo want to start game? y/n:")))
 
-first_step = str(input("Do yo want to start game? y/n:"))
-
-if first_step == "y":
-	print("You go to old forest, and you will have two ways: ")
-else:
-	print("You do not continue the game, ok see you later")
-	exit()
-
-
-way = randint(1, 9)
-human = -1
-
+#The next step, we g
 print("You must change your fate")
-if human != way:
-	human = int(input("try your luck, choose a number from 1 to 9:"))
+
+def battle(human):
 
 	if human <= 5:
 		print("You will go to the left and you find wild beast, he attack you")
-		fight = randint(1, 9)
-		first_try = -1
-
-		if first_try != fight:
-			first_try = int(input("try your strength, choose a number from 1 to 9:"))
+		def beast(first_try):
 			if first_try >= 5:
-				print("You win go to the castle")
-			else:
-				print("You lose, sorry...")
-				exit()
-				pass
-		pass
-	else:
-		print("Welcome to the castle")
-	pass
-pass
+				return("You win go to the castle")
+			print("You have lost, sorry ... you go on, but you only have half lives")
+		beast(int(input("try your strength, choose a number from 1 to 9:")))
+	print("Welcome to the castle")
+
+battle(int(input("try your luck, choose a number from 1 to 9:")))
 
 print("Welcome to the second part our game")
 print("When you stay under the door to the castle, You see a large encoded door lock")
 print("this door lock has a code of 10 numbers, you need to open these doors. To do this, you need to find a number that has 3 digits")
 
-code = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in code:
-	print(str(i) + "0")
 
-your_code = int(input("you need to enter the code: "))
-if your_code == 100:
+def out_door(your_code):
+
+	while your_code != 5:
+		return("this is not the correct answer, one more please")
 	print("You guessed, you can go")
-else:
-	print("this is not the correct answer")
-	exit()
-	pass
+
+out_door(int(input("you need to enter the code from 1 to 9: ")))
+
 print("You go inside the big room and see the owner of the castle")
 print("He asked you:")
 
-ask1 = input("You will want continue the game, y/n?")
+def question(ask1):
+	if ask1 == "y":
+		return("You are can continue game and go to the second room")
+	quit()
 
-if ask1 == "y":
-	print("You are can continue game and go to the second room")
-else:
-	print("youy game is finished")
-	pass
+question(str(input("You will want continue the game, y/n?")))
+
 print("You inside second room")
 
-player = input("You see one guy, he tell you: Do you want play with me? y/n: ")
-
-if player == "y":
-	print("Ok, let's go to play")
-else:
+def easy_play(player):
+	if player == "y":
+		return("Ok, let's go to play")
 	print("Ok, you can go out from the castle, but you don't have any award")
-	pass
+
+easy_play(input("You see one guy, he tell you: Do you want play with me, you need to guess the number?y/n: "))
 
 print("GAME")
 
-human_player = randint(1, 14)
+def rand_number(human_player):
+	lucky = random.randrange(1, 10)
+	print(lucky)
+	if human_player == lucky:
+		return("You guessed, nice game")
+		quit()
+	print("you mistaken try again")
+	return(rand_number(int(input("What Your lucky number?: "))))
 
-human_try = 4
-while human_player != human_try:
-	human_try_guess = int(input("you lucky number from 1 to 14: "))
-	if human_try_guess != human_try:
-		print("you wrong")
-	else:
-		print("You guessed, congratulation, you pick up a gold ducky, this is you award. Good by")
-		exit()
-		pass
-	pass
+print(rand_number(int(input("What Your lucky number?: "))))
