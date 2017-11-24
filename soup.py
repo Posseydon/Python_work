a00710ca9ab0c1912b4dedb866769086b0731fb5
Posseydon <posseydon87@gmail.com>
2017-11-24 13:24:53 +0200

    Update soup.py

--- soup.py
+++ soup.py
@@ -2,16 +2,20 @@ from collections import Counter
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+#we take a function from another file favorite_authors.py, downloading your favorite books
 
 from favorite_authors import poem
 
+#we take new class for our script
 class TextParser:
 
+    #parser from html in text our favorite books
     def get_file(self):
         html_doc = open('/home/posseydon/Desktop/git_tutorial/work/favorite_authors/source_code/0600851.txt').read()
         self.soup = BeautifulSoup(html_doc)
         return self.soup
 
+    #we clean up our text from some bad symbols
     def get_list_of_words(self):
 
         main_text = str(self.soup.html.pre)
@@ -24,6 +28,7 @@ class TextParser:
         #print(self.list_of_words)
         return self.list_of_words
 
+    #Find all words that have 5 letters or more
     def get_long_words(self):
 
         self.long_words = []
@@ -33,6 +38,7 @@ class TextParser:
         #print(self.long_words)
         return self.long_words
 
+    #count how often these words are found in the texts
     def count_all_words(self):
 
         self.most_common_words = {}
@@ -43,6 +49,7 @@ class TextParser:
         #print(self.most_common_words)
         return self.most_common_words
 
+    #write the results in new file 
     def write_result_in_new_file(self):
 
         with open("/home/posseydon/Desktop/git_tutorial/work/favorite_authors/source_code/results.txt", "w") as _file:
