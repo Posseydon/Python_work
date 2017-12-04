2b759d7a23adae0fbbf32374fcf448c0f94db3cf
Posseydon <posseydon87@gmail.com>
2017-12-04 12:10:08 +0200

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,33 +1,34 @@
 import time
 
 x = 0
-while x < 1000000:
-    start_iteration = time.time()
-
-    x += 1
-    #print(x)
+start_iteration = time.time()
+def func_whileloop(x):
+    while x < 1000000:
+        x += 1
 
 finish_iteration = time.time()
 
 print("Count time: " + str(finish_iteration - start_iteration))
 
 start_iteration_for = time.time()
-for item in range(1, 1000000):
-    item += 1.01
-    #print(item)
+def func_forloop():
+    for item in range(1, 1000000):
+        pass
 finish_iteration_for = time.time()
 
 print("Count time: " + str(finish_iteration_for - start_iteration_for))
 
 start_iteration_list = time.time()
-[i * 1 for i in range(1, 1000000)]
+def func_iterlist():
+    [i for i in range(1, 1000000)]
 finish_iteration_list = time.time()
 
 print("Count time: " + str(finish_iteration_list - start_iteration_list))
 
 start_iteration_set = time.time()
 
-{y for y in range(1, 1000000)}
+def func_iterset():
+    {y for y in range(1, 1000000)}
 
 finish_iteration_set = time.time()
 
@@ -35,9 +36,15 @@ print("Count time: " + str(finish_iteration_set - start_iteration_set))
 
 
 start_iteration_dict = time.time()
-letters = ['a', 'b', 'c']
-{a : b for a in letters for b in range(1, 1000000)}
+def func_iterdict():
+    {n: n for n in range(1, 1000000)}
 
 finish_iteration_dict = time.time()
 
 print("Count time: " + str(finish_iteration_dict - start_iteration_dict))
+
+func_whileloop(0)
+func_forloop()
+func_iterlist()
+func_iterset()
+func_iterdict()
